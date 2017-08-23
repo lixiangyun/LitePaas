a41a27c38740a4f3a8e8e9c55a662f16a143a4f5
Panda <linimbus@126.com>
2017-08-23 22:55:24 +0800

    add test function

--- example/3rd/etcd/kv_test/main.go
+++ example/3rd/etcd/kv_test/main.go
@@ -3,13 +3,60 @@ package main
 import (
 	"LitePaas/3rd/etcd/src"
 	"fmt"
+	"time"
 )
+
+var kvs *etcd.EtcdClient
+
+func banchmark(num int) {
+
+	t1 := time.Now()
+
+	for i := 0; i < num; i++ {
+		value := fmt.Sprint("helloworld_%d", i)
+		err := kvs.NewKv("web", value)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+
+	t2 := time.Now()
+
+	subtime := t2.Sub(t1)
+
+	fmt.Println("Time: ", subtime.Seconds())
+	fmt.Println("Speed: ", float64(num)/subtime.Seconds())
+}
+
+func banchmark2(num int) {
+
+	t1 := time.Now()
+
+	for i := 0; i < num; i++ {
+
+		_, err := kvs.GetKv("web")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+
+	t2 := time.Now()
+
+	subtime := t2.Sub(t1)
+
+	fmt.Println("Time: ", subtime.Seconds())
+	fmt.Println("Speed: ", float64(num)/subtime.Seconds())
+}
 
 func main() {
 
 	addr := []string{"localhost:2379"}
 
-	kvs := etcd.NewClient(addr)
+	kvs = etcd.NewClient(addr)
+
+	data := make([]etcd.KvInfo, 0)
 
 	err := kvs.NewKv("web", "helloworld")
 	if err != nil {
@@ -17,7 +64,7 @@ func main() {
 		return
 	}
 
-	data, err := kvs.GetKv("web")
+	data, err = kvs.GetKv("web")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -26,4 +73,6 @@ func main() {
 	fmt.Println("Items:", len(data))
 	fmt.Println("KV: ", data)
 
+	banchmark(3000)
+	banchmark2(3000)
 }
